2d54267572d2e88f5ba51f6304c5858c0fee8d1e
Yuri-Mojito <lukerbroser1@gmail.com>
2021-03-17 22:26:28 -0600

    First commit

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-x=1
-y=2
-z=3
+import amino
+
+comId = "xxxxxxxx"
+email="xxxxxxxxxxxxx@gmail.com"
+password="xxxxxxxxxxx"
+client = amino.client.Client(certificatePath=None, socket_trace=True, socketDebugging=True)
+client.login(email=email,password=password)
+print('\n Login...')
+subclient = amino.SubClient(comId=comId,profile=client.profile)
+print('\n Joined in community...')
+
+@client.event("on_text_message")
+def on_text_message(data):
+    #print(f"{data.message.author.nickname}: {data.message.content}")
